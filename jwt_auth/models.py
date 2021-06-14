355884ce4a3b9395fe3e699eb2fa21e016b3243c
sirdan <danielsdfullerton@gmail.com>
2021-06-14 14:31:44 +0100

    added user relationship models

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -10,4 +10,93 @@ class User(AbstractUser):
         default=2500,
         validators=[MinValueValidator(0), MaxValueValidator(5000)]
     )
+
+    DPS = 'DPS'
+    TANK = 'TANK'
+    SUPPORT = 'SUPPORT'
+    ROLE_CHOICES = [
+        (DPS, 'dps'),
+        (TANK, 'tank'),
+        (SUPPORT, 'support')
+    ]
     
+    mainrole = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=DPS
+    )
+
+    dps1 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='dps1_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be dps',
+        on_delete=models.SET_NULL
+    )
+    dps2 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='dps2_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be dps',
+        on_delete=models.SET_NULL
+    )
+    dps3 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='dps3_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be dps',
+        on_delete=models.SET_NULL
+    )
+
+    tank1 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='tank1_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be tank',
+        on_delete=models.SET_NULL
+    )
+    tank2 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='tank2_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be tank',
+        on_delete=models.SET_NULL
+    )
+    tank3 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='tank3_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be tank',
+        on_delete=models.SET_NULL
+    )
+
+    support1 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='support1_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be support',
+        on_delete=models.SET_NULL
+    )
+    support2 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='support2_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be support',
+        on_delete=models.SET_NULL
+    )
+    support3 = models.ForeignKey(
+        'heroes.Hero',
+        related_name='support3_heroes',
+        blank=True,
+        null=True,
+        help_text='Must be unique, must be support',
+        on_delete=models.SET_NULL
+    )
