f7b89955e8ab3e7cdd2404a1b6f71e68b1926015
sirdan <danielsdfullerton@gmail.com>
2021-07-14 13:54:14 +0100

    deploy prep

--- jwt_auth/serializers.py
+++ jwt_auth/serializers.py
@@ -1,4 +1,5 @@
 
+from heroes.serializers import HeroSerializer
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 # import django.contrib.auth.password_validation as validation
